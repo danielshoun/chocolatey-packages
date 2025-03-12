0d495ba94bb7e0194817301d8e4483c446046a54
Daniel Shoun <github.diffusion@shoun.dev>
2025-03-11 19:16:39 -0600

    Update alire to 2.1.0

--- alire/tools/chocolateyinstall.ps1
+++ alire/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/alire-project/alire/releases/download/v2.0.2/alr-2.0.2-bin-x86_64-windows.zip' # download url, HTTPS preferred
+$url64      = 'https://github.com/alire-project/alire/releases/download/v2.1.0/alr-2.1.0-bin-x86_64-windows.zip' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   url64          = $url64
-  checksum64     = 'a6894e51acbc37af69316d807821917e246dfa8a2bdf5630fd547ff624011286'
+  checksum64     = '373ed06114f46c80e8a10c00a5edb3a14a86c9ad1ce6326060d734968a8155d9'
   checksumType64 = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 
