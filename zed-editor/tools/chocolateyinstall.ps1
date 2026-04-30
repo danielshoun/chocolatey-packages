98c75da87a3ce444a2442e9429ffc37cc6464cbb
Daniel Shoun <github.diffusion@shoun.dev>
2026-04-29 18:39:34 -0600

    [zed-editor] Update package to 1.0.0 [zed-editor] Change iconURL to correct PNG.

--- zed-editor/tools/chocolateyinstall.ps1
+++ zed-editor/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/zed-industries/zed/releases/download/v0.233.10/Zed-x86_64.exe'
+﻿$ErrorActionPreference = 'Stop'
+$url64      = 'https://github.com/zed-industries/zed/releases/download/v1.0.0/Zed-x86_64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url64          = $url64
   softwareName   = 'Zed*'
-  checksum64     = '1a450e3e2b88b388af028ac34567d019f97a0cf8f72c3e2f871d532d7237ab7c'
+  checksum64     = 'f93014dc720ad772d43db8e9b2b705ccdb2f3adb665f4bfbf184f8d8d872a16d'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
