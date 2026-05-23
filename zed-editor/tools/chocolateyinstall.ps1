1d149251762a7d7d868335f7debe91c743cafe6e
Daniel Shoun <github.diffusion@shoun.dev>
2026-05-23 10:27:02 -0600

    [zed-editor] Update package to 1.3.6

--- zed-editor/tools/chocolateyinstall.ps1
+++ zed-editor/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/zed-industries/zed/releases/download/v1.0.0/Zed-x86_64.exe'
+$url64      = 'https://github.com/zed-industries/zed/releases/download/v1.3.6/Zed-x86_64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url64          = $url64
   softwareName   = 'Zed*'
-  checksum64     = 'f93014dc720ad772d43db8e9b2b705ccdb2f3adb665f4bfbf184f8d8d872a16d'
+  checksum64     = '28a5c5d2b1a54e9dba1093d860cc02664044b719dc995af7f2af60cd235b6a3d'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
