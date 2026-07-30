4d7db6f880c1ccbde8fa90da30652ae21f57ca20
danielshoun <36313894+danielshoun@users.noreply.github.com>
2026-07-30 02:14:43 +0000

    Update zed-editor package to v1.13.1

--- zed-editor/tools/chocolateyinstall.ps1
+++ zed-editor/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 $ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/zed-industries/zed/releases/download/v1.12.1/Zed-x86_64.exe'
+$url64      = 'https://github.com/zed-industries/zed/releases/download/v1.13.1/Zed-x86_64.exe'
 $zedCliPath = Join-Path $env:LOCALAPPDATA 'Programs\Zed\bin\zed.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url64          = $url64
   softwareName   = 'Zed*'
-  checksum64     = '443a94d86011c4b5f39f701543e25998c6478bd18b8357486cea9282faf27597'
+  checksum64     = '5699c1651fff0e3bfe7dac1480e45bc8e405f40ea288b19071f0186209809f53'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
